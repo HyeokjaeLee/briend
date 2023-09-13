6725639eb0f0b429d9137b7e406cd19354538fd5
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-09-14 04:18:03 +0900

    fix: indexedDB 버그 수정
    - 의도하지 않은 비동기적 동작 예외처리

--- src/store/useChattingRoomIndexDBStore.ts
+++ src/store/useChattingRoomIndexDBStore.ts
@@ -1,16 +1,10 @@
 import { createWithEqualityFn } from 'zustand/traditional';
 
-import { LOCAL_STORAGE } from '@/constants';
 import { Message, ChattingRoom } from '@/types';
 
 import type { Channel } from 'pusher-js';
 
 const CHATTING_ROOM_STORE_NAME = 'chatting-room';
-
-enum UPGRADE_TYPE {
-  CREATE = 'create',
-  DELETE = 'delete',
-}
 
 interface ChattingRoomIndexDBStore {
   mountDB: (userId?: string | null) => void;
@@ -29,50 +23,49 @@ interface ChattingRoomIndexDBStore {
 }
 
 export const useChattingRoomIndexDBStore =
-  createWithEqualityFn<ChattingRoomIndexDBStore>((set, get) => {
+  createWithEqualityFn<ChattingRoomIndexDBStore>((set) => {
     let DBName: string | undefined;
-    let DBVersion: number;
-    const increaseDBVersion = () => {
-      DBVersion += 1;
-      window.localStorage.setItem(
-        LOCAL_STORAGE.INDEXED_DB_VERSION,
-        String(DBVersion),
-      );
-      return DBVersion;
-    };
-
-    let upgradeNeededArgs: {
-      type: UPGRADE_TYPE;
-      token: string;
-    } | null = null;
-
-    const getMessageStoreName = (token: string) => `message-${token}`;
 
     let request: IDBOpenDBRequest | undefined;
 
     let db: IDBDatabase | undefined;
 
-    const checkChattingRoomStoreExist = (db: IDBDatabase) =>
-      db.objectStoreNames.contains(CHATTING_ROOM_STORE_NAME);
+    const getChattingRoomStore = () => {
+      if (!db) throw new Error('db가 존재하지 않습니다.');
+      return db
+        .transaction(CHATTING_ROOM_STORE_NAME, 'readwrite')
+        .objectStore(CHATTING_ROOM_STORE_NAME);
+    };
 
-    const checkMessageStoreExist = (
-      db: IDBDatabase,
-      messageStoreName: string,
-    ) => db.objectStoreNames.contains(messageStoreName);
+    const update = (onUpdate: (db: IDBDatabase) => () => void) => {
+      if (!db || !DBName)
+        throw new Error('db 혹은 DBName이 존재하지 않습니다.');
+
+      db.close();
+
+      request = indexedDB.open(DBName, db.version + 1);
+
+      let handleSuccess: () => void;
+
+      request.onupgradeneeded = (e) => {
+        db = (e.target as IDBOpenDBRequest).result;
+
+        handleSuccess = onUpdate(db);
+      };
+
+      request.onsuccess = () => handleSuccess();
+    };
 
     return {
       mountDB: (userId) => {
-        DBVersion = Number(
-          window.localStorage.getItem(LOCAL_STORAGE.INDEXED_DB_VERSION) ?? 1,
-        );
-
         DBName = `${userId ?? 'guest'}-db`;
 
-        request = indexedDB.open(DBName, DBVersion);
+        request = indexedDB.open(DBName);
 
         request.onsuccess = (e) => {
           db = (e.target as IDBOpenDBRequest).result;
-          if (checkChattingRoomStoreExist(db)) {
+
+          if (db.objectStoreNames.contains(CHATTING_ROOM_STORE_NAME)) {
             const chattingRoomStore = db
               .transaction(CHATTING_ROOM_STORE_NAME, 'readonly')
               .objectStore(CHATTING_ROOM_STORE_NAME);
@@ -82,52 +75,16 @@ export const useChattingRoomIndexDBStore =
 
               set({ chattingRoomList: result });
             };
-          } else if (DBName)
-            request = indexedDB.open(DBName, increaseDBVersion());
+          }
         };
 
         request.onupgradeneeded = (e) => {
-          const { result: db } = e.target as IDBOpenDBRequest;
+          db = (e.target as IDBOpenDBRequest).result;
 
-          if (!checkChattingRoomStoreExist(db)) {
+          if (!db.objectStoreNames.contains(CHATTING_ROOM_STORE_NAME)) {
             db.createObjectStore(CHATTING_ROOM_STORE_NAME, {
               keyPath: 'token',
             });
-          }
-
-          //* messageStore 생성 및 삭제
-          if (upgradeNeededArgs) {
-            const { type, token } = upgradeNeededArgs;
-
-            const messageStoreName = getMessageStoreName(token);
-
-            const isMessageStoreExist = checkMessageStoreExist(
-              db,
-              messageStoreName,
-            );
-
-            switch (type) {
-              case UPGRADE_TYPE.CREATE: {
-                if (!isMessageStoreExist) {
-                  // TODO: 여기 못들어오고 있음
-                  console.log('test');
-                  db.createObjectStore(messageStoreName, {
-                    keyPath: 'id',
-                    autoIncrement: true,
-                  });
-                }
-
-                break;
-              }
-
-              case UPGRADE_TYPE.DELETE: {
-                if (isMessageStoreExist) db.deleteObjectStore(messageStoreName);
-
-                break;
-              }
-            }
-
-            upgradeNeededArgs = null;
           }
         };
       },
@@ -137,82 +94,65 @@ export const useChattingRoomIndexDBStore =
           throw new Error('params 혹은 db가 존재하지 않습니다.');
 
         const { token, channel } = params;
-        const messageStoreName = getMessageStoreName(token);
 
-        if (
-          checkMessageStoreExist(db, messageStoreName) &&
-          checkChattingRoomStoreExist(db)
-        ) {
-          const transaction = db.transaction(
-            [CHATTING_ROOM_STORE_NAME, messageStoreName],
-            'readonly',
-          );
+        const transaction = db.transaction(
+          [CHATTING_ROOM_STORE_NAME, token],
+          'readonly',
+        );
 
-          transaction
-            .objectStore(CHATTING_ROOM_STORE_NAME)
-            .get(token).onsuccess = (e) => {
-            const { result } = e.target as IDBRequest<ChattingRoom>;
+        transaction.objectStore(CHATTING_ROOM_STORE_NAME).get(token).onsuccess =
+          (e) => {
+            const { result: chattingRoom } =
+              e.target as IDBRequest<ChattingRoom>;
 
             set({
               chattingRoom: {
-                ...result,
+                ...chattingRoom,
                 channel,
               },
             });
           };
 
-          transaction.objectStore(messageStoreName).getAll().onsuccess = (
-            e,
-          ) => {
-            const { result } = e.target as IDBRequest<Message[]>;
+        transaction.objectStore(token).getAll().onsuccess = (e) => {
+          const { result: messageList } = e.target as IDBRequest<Message[]>;
 
-            set({ messageList: result });
-          };
-        }
+          set({ messageList });
+        };
       },
 
       createChattingRoom: (chattingRoom) => {
-        if (!request || !db || !DBName) {
-          throw new Error(
-            'chattingRoom을 생성하기 위한 요소가 존재하지 않습니다.',
-          );
-        }
+        const chattingRoomStore = getChattingRoomStore();
 
-        const { token } = chattingRoom;
+        chattingRoomStore.add(chattingRoom).onsuccess = () => {
+          chattingRoomStore.getAll().onsuccess = (e) => {
+            const { result } = e.target as IDBRequest<ChattingRoom[]>;
 
-        if (!checkMessageStoreExist(db, getMessageStoreName(token))) {
-          upgradeNeededArgs = {
-            type: UPGRADE_TYPE.CREATE,
-            token,
+            update((db) => {
+              db.createObjectStore(chattingRoom.token, {
+                keyPath: 'id',
+                autoIncrement: true,
+              });
+
+              return () => set({ chattingRoomList: result });
+            });
           };
-
-          request = indexedDB.open(DBName, increaseDBVersion());
-        }
-
-        db.transaction(CHATTING_ROOM_STORE_NAME, 'readwrite')
-          .objectStore(CHATTING_ROOM_STORE_NAME)
-          .add(chattingRoom);
+        };
       },
 
       deleteChattingRoom: (token) => {
-        if (!request || !db || !DBName) {
-          throw new Error(
-            'chattingRoom을 삭제하기 위한 요소가 존재하지 않습니다.',
-          );
-        }
+        const chattingRoomStore = getChattingRoomStore();
 
-        db.transaction(CHATTING_ROOM_STORE_NAME, 'readwrite')
-          .objectStore(CHATTING_ROOM_STORE_NAME)
-          .delete(token);
+        chattingRoomStore.delete(token).onsuccess = () => {
+          chattingRoomStore.getAll().onsuccess = (e) => {
+            const { result } = e.target as IDBRequest<ChattingRoom[]>;
 
-        if (!checkMessageStoreExist(db, getMessageStoreName(token))) {
-          upgradeNeededArgs = {
-            type: UPGRADE_TYPE.DELETE,
-            token,
+            update((db) => {
+              db.deleteObjectStore(token);
+
+              return () => set({ chattingRoomList: result });
+            });
           };
-
-          request = indexedDB.open(DBName, increaseDBVersion());
-        }
+        };
       },
     };
   }, Object.is);
