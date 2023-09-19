ccb240dc0df68d815a6730055ddda5dd5c0e160b
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-09-20 02:01:57 +0900

    feat: 초기 입장 Toast 제거

--- src/constants/language.ts
+++ src/constants/language.ts
@@ -50,15 +50,6 @@ export const LANGUAGE_PACK = {
       `このチャットは期限切れです。\n${opponentName}に新しいリンクを要求してください！`,
   },
 
-  JOIN_NEW_CHATTING_ROOM_TOAST: {
-    [LANGUAGE.KOREAN]: (opponentName: string) =>
-      `${opponentName}님과 대화를 시작해보세요!\n한국어로 말하면 번역해드려요!`,
-    [LANGUAGE.ENGLISH]: (opponentName: string) =>
-      `Start chatting with ${opponentName}!\nChat English and I'll translate it!`,
-    [LANGUAGE.JAPANESE]: (opponentName: string) =>
-      `${opponentName}とチャットを始めましょう！\n日本語で話して、私が翻訳します！`,
-  },
-
   CHATTING_ROOM_GUEST_TITLE: {
     [LANGUAGE.KOREAN]: '채팅방에 초대 되었어요!',
     [LANGUAGE.ENGLISH]: 'You have been invited to the chat!',
