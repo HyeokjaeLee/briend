4f2da8017850adf04b0cff1421e7d8e6d5847871
HyeokjaeLee <leehyeokjae97@gmail.com>
2023-09-26 12:38:17 +0900

    feat: Analytics 추가
    - Google Search console 스크립트 추가
    - Vercel Analytics 스크립트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import '@hyeokjaelee/pastime-ui/style.css';
 
 import type { Metadata } from 'next';
 
+import { Analytics } from './layout/components/Analytics';
 import { GlobalMenu } from './layout/components/GlobalMenu';
 import { GlobalNav } from './layout/components/GlobalNav';
 import { GlobalProvider } from './layout/components/GlobalProvider';
@@ -36,6 +37,7 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => (
   <html lang="ko" className="text-sm sm:text-base w-full h-full">
+    <Analytics />
     <GlobalProvider>
       <body className="bg-slate-100 dark:bg-zinc-800 text-zinc-600 dark:text-slate-300 font-pretendard break-keep font-medium w-full h-full flex flex-col overflow-hidden">
         <GlobalNav />
