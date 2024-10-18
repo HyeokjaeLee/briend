f305b4a24458216b6a77204711f8c7692ecccf21
HyeokjaeLee <leehyeokjae97@gmail.com>
2024-10-18 12:31:49 +0900

    fix: lint

--- src/app/api/edit-profile/route.ts
+++ src/app/api/edit-profile/route.ts
@@ -6,14 +6,15 @@ import type { ApiParams, ApiResponse } from '@/types/api';
 import { createApiRoute } from '@/utils/createApiRoute';
 import { CustomError, ERROR } from '@/utils/customError';
 
-export const POST = createApiRoute(async (req: NextRequest) => {
-  const params: ApiParams.EDIT_PROFILE = await req.json();
+export const POST = createApiRoute(
+  async (req: NextRequest) => {
+    const params: ApiParams.EDIT_PROFILE = await req.json();
 
-  const id = req.cookies.get(COOKIES.USER_ID)?.value;
+    const id = req.cookies.get(COOKIES.USER_ID)?.value;
 
-  if (!id) throw new CustomError(ERROR.NOT_ENOUGH_PARAMS(['id']));
+    if (!id) throw new CustomError(ERROR.NOT_ENOUGH_PARAMS(['id']));
 
-  const user = await prisma.users.update({
+    const user = await prisma.users.update({
       where: {
         id,
       },
@@ -23,7 +24,8 @@ export const POST = createApiRoute(async (req: NextRequest) => {
       },
     });
 
-    if (!user.name) throw new CustomError(ERROR.NOT_ENOUGH_PARAMS(['user.name']));
+    if (!user.name)
+      throw new CustomError(ERROR.NOT_ENOUGH_PARAMS(['user.name']));
 
     return NextResponse.json<ApiResponse.EDIT_PROFILE>({
       emoji: user.emoji,
