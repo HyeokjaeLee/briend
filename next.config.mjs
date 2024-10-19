4cbf80c6325b400744f70898ac57ebdc52cd51e5
HyeokjaeLee <leehyeokjae97@gmail.com>
2024-10-19 14:43:39 +0900

    fix: 인증 환경 변수 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    AUTH_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
