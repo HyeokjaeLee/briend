288bef3270ec37ae2a4db759114bc293a9872ccc
HyeokjaeLee <leehyeokjae97@gmail.com>
2024-10-21 02:06:27 +0900

    fix: 로그아웃 콜백 로직 삭제

--- src/app/[lng]/_components/LogoutCallback.tsx
+++ src/app/[lng]/_components/LogoutCallback.tsx
@@ -1,18 +1,14 @@
 'use client';
 
-import { nanoid } from 'nanoid';
-
 import { useEffect } from 'react';
-import { useCookies } from 'react-cookie';
 
 import { useTranslation } from '@/app/i18n/client';
-import { COOKIES } from '@/constants/cookies-key';
+
 import { SESSION_STORAGE } from '@/constants/storage-key';
 import { toast } from '@/utils/toast';
 
 export const LogoutCallback = () => {
   const { t } = useTranslation('layout');
-  const [, setCookie] = useCookies([COOKIES.USER_ID]);
 
   useEffect(() => {
     const isLogouted =
@@ -24,8 +20,6 @@ export const LogoutCallback = () => {
       toast({
         message: t('logout-toast-message'),
       });
-
-      setCookie(COOKIES.USER_ID, nanoid());
     }
   }, [t, setCookie]);
 
