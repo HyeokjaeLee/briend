0f50b8b1fc605d0666b576a2b4689fcda3be6ae6
HyeokjaeLee <leehyeokjae97@gmail.com>
2024-10-21 23:38:21 +0900

    fix: 로그아웃 콜백 수정

--- src/app/[lng]/private/more/_components/LogoutButton.tsx
+++ src/app/[lng]/private/more/_components/LogoutButton.tsx
@@ -1,33 +1,13 @@
 'use client';
 
-import { nanoid } from 'nanoid';
-
-import { useEffect, useState } from 'react';
-import { useCookies } from 'react-cookie';
 import { RiLogoutBoxRLine } from 'react-icons/ri';
 
 import { useTranslation } from '@/app/i18n/client';
 import { CustomButton } from '@/components/CustomButton';
-import { COOKIES } from '@/constants/cookies-key';
 import { toast } from '@/utils/toast';
 
 export const LogoutButton = () => {
   const { t } = useTranslation('more');
-  const [, setCookie] = useCookies([COOKIES.USER_ID]);
-
-  const [isLogouting, setIsLogouting] = useState(false);
-
-  useEffect(() => {
-    if (!isLogouting) return;
-
-    return () => {
-      setIsLogouting(false);
-      setCookie(COOKIES.USER_ID, nanoid());
-      toast({
-        message: t('logout-toast-message'),
-      });
-    };
-  }, [isLogouting, setCookie, t]);
 
   return (
     <CustomButton
@@ -35,7 +15,9 @@ export const LogoutButton = () => {
       type="submit"
       variant="ghost"
       onClick={() => {
-        setIsLogouting(true);
+        toast({
+          message: t('logout-toast-message'),
+        });
       }}
     >
       {t('logout')}
