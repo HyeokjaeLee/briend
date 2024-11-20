436145251d3a486cd0d5302327ac0b4067e5d1e8
HyeokjaeLee <leehyeokjae97@gmail.com>
2024-11-21 01:35:30 +0900

    refactor: i18n 리다이렉팅 로직 개선
    - 쿠키 설정 버그 수정

--- src/app/i18n/client.ts
+++ src/app/i18n/client.ts
@@ -8,7 +8,6 @@ import resourcesToBackend from 'i18next-resources-to-backend';
 import { useParams } from 'next/navigation';
 
 import { useLayoutEffect, useState } from 'react';
-import { useCookies } from 'react-cookie';
 import type {
   UseTranslationOptions,
   UseTranslationResponse,
@@ -19,7 +18,6 @@ import {
   useTranslation as useOriginalTranslation,
 } from 'react-i18next';
 
-import { COOKIES } from '@/constants/cookies-key';
 import { CustomError, ERROR } from '@/utils/customError';
 
 import { getOptions, languages } from './settings';
@@ -50,7 +48,6 @@ export const useTranslation = <
   ns?: Ns,
   options?: UseTranslationOptions<KPrefix>,
 ): UseTranslationResponse<FallbackNs<Ns>, KPrefix> => {
-  const [cookies, setCookie] = useCookies([COOKIES.I18N]);
   const translation = useOriginalTranslation(ns, options);
   const { i18n } = translation;
   const lng = useParams().lng;
@@ -66,20 +63,15 @@ export const useTranslation = <
 
   useLayoutEffect(() => {
     if (activeLng === i18n.resolvedLanguage) return;
+
     setActiveLng(i18n.resolvedLanguage);
   }, [activeLng, i18n.resolvedLanguage]);
 
   useLayoutEffect(() => {
     if (!lng || i18n.resolvedLanguage === lng) return;
+
     i18n.changeLanguage(lng);
   }, [lng, i18n]);
-
-  useLayoutEffect(() => {
-    console.info(cookies.i18next, lng);
-    if (cookies.i18next === lng) return;
-
-    setCookie(COOKIES.I18N, lng, { path: '/' });
-  }, [lng, cookies.i18next, setCookie]);
 
   return translation;
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,8 @@
-import acceptLanguage from 'accept-language';
 import { type NextRequest, NextResponse } from 'next/server';
 
-import { fallbackLng, languages } from './app/i18n/settings';
 import { auth } from './auth';
-import { COOKIES } from './constants/cookies-key';
-import { setUserIdCookie } from './utils/api/setUserIdCookie';
+import { setI18nNextResponse } from './middleware/setI18nNextResponse';
+import { setUserIdCookie } from './middleware/setUserIdCookie';
 
 export const config = {
   matcher: [
@@ -12,42 +10,12 @@ export const config = {
   ],
 };
 
-acceptLanguage.languages(languages);
-
 export const middleware = auth(async (req: NextRequest) => {
   const { nextUrl } = req;
 
-  //* 🌍 i18n redirect 🌍
-  let lng;
-  const i18nCookie = req.cookies.get(COOKIES.I18N);
+  if (nextUrl.pathname.startsWith('/_next')) return NextResponse.next();
 
-  if (i18nCookie) lng = acceptLanguage.get(i18nCookie.value);
-  if (!lng)
-    lng = acceptLanguage.get(req.headers.get('Accept-Language')) || fallbackLng;
-
-  if (
-    !languages.some((loc) => {
-      const langPath = `/${loc}`;
-
-      const { pathname } = nextUrl;
-
-      return (
-        pathname.startsWith(langPath + '/') ||
-        //* root path
-        nextUrl.pathname === langPath
-      );
-    }) &&
-    !nextUrl.pathname.startsWith('/_next')
-  ) {
-    nextUrl.pathname = `/${lng}${nextUrl.pathname}`;
-
-    return NextResponse.redirect(nextUrl);
-  }
-  const res = NextResponse.next();
-
-  const purePath = nextUrl.pathname.replace(`/${lng}`, '');
-
-  res.headers.set('pure-path', purePath);
+  const res = setI18nNextResponse(req);
 
   setUserIdCookie(req, res);
 
