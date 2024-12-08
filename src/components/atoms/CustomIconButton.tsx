a6572b2e3125ba1f0027e5fb418ad78f41d32c08
HyeokjaeLee <leehyeokjae97@gmail.com>
2024-12-08 13:14:19 +0900

    refactor: RootNav 반응 타이밍 수정

--- src/components/atoms/CustomIconButton.tsx
+++ src/components/atoms/CustomIconButton.tsx
@@ -1,19 +1,33 @@
 import { cn } from '@/utils/cn';
 import { IconButton, type IconButtonProps } from '@radix-ui/themes';
 
+interface CustomIconButtonProps extends IconButtonProps {
+  activeScaleDown?: boolean;
+}
+
 export const CustomIconButton = ({
   className,
   color = 'blue',
+  type = 'button',
+  size = '4',
+  activeScaleDown = true,
   ...restProps
-}: IconButtonProps) => {
+}: CustomIconButtonProps) => {
   return (
     <IconButton
+      {...restProps}
       className={cn(
         'disabled:cursor-not-allowed enabled:cursor-pointer outline-none',
+        {
+          'active:scale-75 active:bg-slate-100 transition-all duration-75':
+            activeScaleDown,
+          'h-14': size === '4',
+        },
         className,
       )}
       color={color}
-      {...restProps}
+      size={size}
+      type={type}
     />
   );
 };
