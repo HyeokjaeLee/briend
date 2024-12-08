6257e49c6e0192b9cdb8b076eff574ad4f91be42
HyeokjaeLee <leehyeokjae97@gmail.com>
2024-12-08 19:23:06 +0900

    draft: 페러럴 라우트가 새로고침 버그 수정
    - 페러럴 라우트가 새로고침시 정상적으로 렌더링 되지 않는 이슈가 있음

--- src/app/[lng]/@loading/page.tsx
+++ src/app/[lng]/@loading/page.tsx
@@ -2,14 +2,14 @@
 
 import { useShallow } from 'zustand/shallow';
 
-import { useEffect, type PropsWithChildren } from 'react';
+import { useLayoutEffect } from 'react';
 
 import { LoadingTemplate } from '@/components/templates/LoadingTemplate';
 import { useUrl } from '@/hooks/useUrl';
 import { useGlobalStore } from '@/stores/global';
 import { cn } from '@/utils/cn';
 
-const GlobalTemplate = ({ children }: PropsWithChildren) => {
+const LoadingPage = () => {
   const [globalLoading, setGlobalLoading] = useGlobalStore(
     useShallow((state) => [state.globalLoading, state.setGlobalLoading]),
   );
@@ -20,28 +20,26 @@ const GlobalTemplate = ({ children }: PropsWithChildren) => {
 
   const url = useUrl();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setGlobalLoading(false);
   }, [url, setGlobalLoading]);
 
   return (
-    <>
-      {isLoading ? (
-        <LoadingTemplate
-          className={cn(
-            'absolute animate-fade bg-slate-200/50 backdrop-blur-xl animate-duration-150',
-            {
-              0: 'animate-delay-0',
-              100: 'animate-delay-100',
-              200: 'animate-delay-200',
-              300: 'animate-delay-300',
-            }[delay],
-          )}
-        />
-      ) : null}
-      {children}
-    </>
+    <LoadingTemplate
+      className={cn(
+        'absolute animate-fade bg-slate-200/50 backdrop-blur-xl animate-duration-150',
+        {
+          0: 'animate-delay-0',
+          100: 'animate-delay-100',
+          200: 'animate-delay-200',
+          300: 'animate-delay-300',
+        }[delay],
+        {
+          '!hidden': !isLoading,
+        },
+      )}
+    />
   );
 };
 
-export default GlobalTemplate;
+export default LoadingPage;
