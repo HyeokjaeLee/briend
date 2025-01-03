e4dc27a06a3a072267a381cb8143ba924a260c28
HyeokjaeLee <leehyeokjae97@gmail.com>
2025-01-04 05:56:28 +0900

    refactor: modal 컴포넌트 개선
    - dialog 태그를 사용하도록 변경

--- src/components/molecules/Modal.tsx
+++ src/components/molecules/Modal.tsx
@@ -1,17 +1,9 @@
-import { AnimatePresence, motion } from 'framer-motion';
-
 import { RiCloseLine } from 'react-icons/ri';
 
 import { cn } from '@/utils';
-import { Portal } from '@radix-ui/themes';
+import { Kbd, Portal } from '@radix-ui/themes';
 
 import { CustomIconButton } from '../atoms/CustomIconButton';
-
-const MODAL_ANIMATION = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  exit: { opacity: 0, transition: { duration: 0.1 } },
-};
 
 export interface ModalProps {
   children: React.ReactNode;
@@ -36,54 +28,64 @@ export const Modal = ({
   };
 
   return (
-    <AnimatePresence>
-      {open ? (
-        <Portal
-          className="fixed inset-0 z-20 bg-zinc-900/50 backdrop-blur-sm"
-          onClick={handleClose}
+    <Portal asChild>
+      <dialog
+        className={cn(
+          'fixed z-20 size-full bg-zinc-900/50 backdrop-blur-sm',
+          'animate-duration-150 animate-fade',
+        )}
+        open={open}
+        onClick={handleClose}
+        onKeyDown={(e) => {
+          if (e.key === 'Escape') onClose?.();
+        }}
+        onMouseEnter={({ target }) => {
+          if ('focus' in target) {
+            (target as HTMLElement).focus();
+          }
+        }}
+      >
+        <Kbd className="absolute right-2 top-2">ESC</Kbd>
+        <article
+          className={cn(
+            'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 shadow-lg',
+            'rounded-lg bg-white',
+          )}
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
         >
-          <motion.article
-            {...MODAL_ANIMATION}
+          {hasCloseButton || title ? (
+            <header className="mx-5 mt-5 flex items-center justify-end">
+              {title ? (
+                <h3 className="w-full text-start text-xl font-semibold">
+                  {title}
+                </h3>
+              ) : null}
+              {hasCloseButton ? (
+                <CustomIconButton
+                  color="gray"
+                  radius="large"
+                  size="3"
+                  variant="ghost"
+                  onClick={handleClose}
+                >
+                  <RiCloseLine className="size-8" />
+                </CustomIconButton>
+              ) : null}
+            </header>
+          ) : null}
+          <section
             className={cn(
-              'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 shadow-lg',
-              'rounded-lg bg-white',
+              'relative m-5 flex flex-col items-center h-full',
+              'min-w-52 min-h-24 max-w-[calc(100%-2rem)] max-h-[calc(100%-2rem)]',
+              className,
             )}
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
           >
-            {hasCloseButton || title ? (
-              <header className="mx-5 mt-5 flex items-center justify-end">
-                {title ? (
-                  <h3 className="w-full text-start text-xl font-semibold">
-                    {title}
-                  </h3>
-                ) : null}
-                {hasCloseButton ? (
-                  <CustomIconButton
-                    color="gray"
-                    radius="large"
-                    size="3"
-                    variant="ghost"
-                    onClick={handleClose}
-                  >
-                    <RiCloseLine className="size-8" />
-                  </CustomIconButton>
-                ) : null}
-              </header>
-            ) : null}
-            <section
-              className={cn(
-                'relative m-5 flex flex-col items-center h-full',
-                'min-w-52 min-h-24 max-w-[calc(100%-2rem)] max-h-[calc(100%-2rem)]',
-                className,
-              )}
-            >
-              {children}
-            </section>
-          </motion.article>
-        </Portal>
-      ) : null}
-    </AnimatePresence>
+            {children}
+          </section>
+        </article>
+      </dialog>
+    </Portal>
   );
 };
