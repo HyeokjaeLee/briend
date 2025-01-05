0eb7f6febd0fa2f40948a513da91233ed3ee41e4
HyeokjaeLee <leehyeokjae97@gmail.com>
2025-01-05 20:39:10 +0900

    feat: drawer 컴포넌트 개선

--- src/app/[lng]/@rightSide/[...dynamicParams]/_components/Empty.tsx
+++ src/app/[lng]/@rightSide/[...dynamicParams]/_components/Empty.tsx
@@ -1,3 +1,17 @@
+import { useTranslation } from 'react-i18next';
+
+import Logo from '@/svgs/logo.svg';
+
 export default function EmptyTemplate() {
-  return <div>Empty</div>;
+  const { t } = useTranslation('layout');
+
+  return (
+    <div className="size-full flex-col gap-4 bg-white flex-center">
+      <Logo className="w-40" />
+      <div className="text-center">
+        <h2 className="mb-1 text-lg font-semibold">{t('empty')}</h2>
+        <p className="text-sm text-gray-500">{t('empty-description')}</p>
+      </div>
+    </div>
+  );
 }
