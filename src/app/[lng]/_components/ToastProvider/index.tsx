14a80a7a8a487c9b40a8cad7e8d20e69c5f3b203
HyeokjaeLee <leehyeokjae97@gmail.com>
2025-02-25 00:29:49 +0900

    chore: 스타일 분리

--- src/app/[lng]/_components/ToastProvider/index.tsx
+++ src/app/[lng]/_components/ToastProvider/index.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import '@/styles/toastify.css';
+
 import { Slide, ToastContainer } from 'react-toastify';
 
 import { cn } from '@/utils';
@@ -14,7 +16,7 @@ export const ToastProvider = () => {
       draggable
       className={cn(
         'h-0 p-0 relative w-[calc(100%-2rem)]',
-        'sm:min-w-80 sm:fixed sm:w-fit',
+        'sm:min-w-80 sm:fixed sm:w-fit xl:w-[calc(100%-2rem)] xl:relative',
       )}
       draggableDirection="x"
       draggablePercent={50}
@@ -24,6 +26,7 @@ export const ToastProvider = () => {
       toastClassName={cn(
         'flex justify-start w-full px-4',
         'rounded-lg backdrop-blur-sm cursor-grab font-pretendard whitespace-pre-line font-medium text-lg',
+        'left-4 xs:left-0',
       )}
       transition={Slide}
     />
