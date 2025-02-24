f06a686e0e8fe40bf3780f685a787b10cccb7038
HyeokjaeLee <leehyeokjae97@gmail.com>
2025-02-25 02:45:07 +0900

    fIx: 레이아웃 버그 수정
    - 키보드 스크롤 버그 수정
    - Toast layout 깨짐 버그 수정

--- src/app/[lng]/_components/GlobalProvider/_components/GlobalListener/_hooks/useScrollListener.ts
+++ src/app/[lng]/_components/GlobalProvider/_components/GlobalListener/_hooks/useScrollListener.ts
@@ -3,11 +3,12 @@ import { useEffect } from 'react';
 export const useScrollListener = () => {
   useEffect(() => {
     const handleScroll = (e: Event) => {
-      e.preventDefault();
       window.scrollTo({ top: 0 });
+      e.stopPropagation();
+      e.preventDefault();
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll, { passive: false });
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
--- src/app/[lng]/_components/GlobalProvider/_components/GlobalListener/index.tsx
+++ src/app/[lng]/_components/GlobalProvider/_components/GlobalListener/index.tsx
@@ -1,11 +1,13 @@
 import { useChattingListener } from './_hooks/useChattingListener';
 import { useHistoryListener } from './_hooks/useHistoryListener';
+import { useScrollListener } from './_hooks/useScrollListener';
 import { useViewportListener } from './_hooks/useViewportListener';
 
 export const GlobalListener = () => {
   useChattingListener();
   useHistoryListener();
   useViewportListener();
+  useScrollListener();
 
   return <></>;
 };
