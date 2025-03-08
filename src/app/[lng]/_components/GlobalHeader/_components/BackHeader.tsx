b8f3664fcf69b3cddc6957e3c9b4e6ce7adddf8c
HyeokjaeLee <leehyeokjae97@gmail.com>
2025-03-09 06:45:19 +0900

    fix: 빌드 에러 수정

--- src/app/[lng]/_components/GlobalHeader/_components/BackHeader.tsx
+++ src/app/[lng]/_components/GlobalHeader/_components/BackHeader.tsx
@@ -4,7 +4,6 @@ import { usePathname } from 'next/navigation';
 import { RiArrowGoBackFill, RiCloseLine } from 'react-icons/ri';
 
 import { Button } from '@/components';
-import { useTranslation } from '@/configs/i18n/client';
 import { useCustomRouter } from '@/hooks';
 import { ROUTES } from '@/routes/client';
 import { cn, findRoute } from '@/utils';
@@ -19,9 +18,6 @@ export const BackHeader = () => {
 
   const BackIcon = isLoginPage ? RiCloseLine : RiArrowGoBackFill;
 
-  const { topHeaderTitle } = currentRoute;
-  const { t } = useTranslation('layout');
-
   return (
     <nav
       className={cn('flex h-14 items-center justify-between px-1', {
@@ -31,11 +27,6 @@ export const BackHeader = () => {
       <Button variant="ghost" onClick={() => router.back()} onlyIcon>
         <BackIcon />
       </Button>
-      {topHeaderTitle ? (
-        <h1 className="text-lg font-semibold text-gray-700">
-          {t(topHeaderTitle)}
-        </h1>
-      ) : null}
     </nav>
   );
 };
