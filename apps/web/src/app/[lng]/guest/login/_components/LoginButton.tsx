711fab5f8c28f8f13b140999053f476feba296a7
HyeokjaeLee <leehyeokjae97@gmail.com>
2025-07-04 01:25:06 +0900

    perf: 미들웨어 제거
    - 로그인 로직 클라이언트 위주로 변경

--- apps/web/src/app/[lng]/guest/login/_components/LoginButton.tsx
+++ apps/web/src/app/[lng]/guest/login/_components/LoginButton.tsx
@@ -1,59 +1,39 @@
 'use client';
 
-import { getAuth } from 'firebase/auth';
 import Image from 'next/image';
 
-import { COOKIES, LOGIN_PROVIDERS } from '@/constants';
-import { useGlobalStore } from '@/stores';
-import { cn, customCookies, CustomError } from '@/utils';
+import type { LOGIN_PROVIDERS } from '@/constants';
+import { LOGIN_PROVIDERS as P } from '@/constants';
+import { cn } from '@/utils';
 
-export interface LoginButtonProps {
+export interface LoginButtonProps
+  extends React.ComponentPropsWithoutRef<'button'> {
   provider: LOGIN_PROVIDERS;
   fullSize?: boolean;
   text?: string;
-  name: string;
 }
 
 export const LoginButton = ({
   provider,
   fullSize,
   text,
-  name,
+  ...rest
 }: LoginButtonProps) => {
-  const setLoading = useGlobalStore((state) => state.setGlobalLoading);
-
   return (
     <button
-      data-slot="button"
+      {...rest}
       className={cn(
         'flex-center h-14 cursor-pointer',
         fullSize
           ? 'font-pretendard w-full rounded-lg px-7 text-lg font-semibold'
           : 'w-14 rounded-full',
         {
-          [LOGIN_PROVIDERS.GOOGLE]:
-            'text-slate-850 border border-slate-200 bg-white',
-          [LOGIN_PROVIDERS.KAKAO]: 'bg-kakao-yellow text-slate-850',
-          [LOGIN_PROVIDERS.NAVER]: 'bg-naver-green',
+          [P.GOOGLE]: 'text-slate-850 border border-slate-200 bg-white',
+          [P.KAKAO]: 'bg-kakao-yellow text-slate-850',
+          [P.NAVER]: 'bg-naver-green',
         }[provider],
       )}
-      name={name}
       value={provider}
-      onClick={(e) => {
-        setLoading(true);
-
-        const { currentUser } = getAuth();
-
-        if (!currentUser?.isAnonymous) {
-          e.preventDefault();
-
-          throw new CustomError('is not anonymous');
-        }
-
-        customCookies.set(COOKIES.ANONYMOUS_ID, currentUser.uid);
-
-        location.href = '/api/auth/external';
-      }}
     >
       <div
         className={
--- apps/web/src/app/[lng]/guest/login/page.tsx
+++ apps/web/src/app/[lng]/guest/login/page.tsx
@@ -1,23 +1,35 @@
-import { Logo } from '@/components';
-import { signIn } from '@/configs/auth';
-import { getTranslation } from '@/configs/i18n/server';
-import { type LANGUAGE, LOGIN_PROVIDERS } from '@/constants';
+'use client';
+
+import { redirect } from 'next/navigation';
+import { signIn, useSession } from 'next-auth/react';
+
+import { PageLoadingTemplate } from '@/components';
+import { useTranslation } from '@/configs/i18n/client';
+import { LOGIN_PROVIDERS } from '@/constants';
+import { ROUTES } from '@/routes/client';
+import Logo from '@/svgs/logo.svg';
 import { assertEnum } from '@/utils';
 
 import { LoginButton } from './_components/LoginButton';
 
-interface LoginPageProps {
-  params: Promise<{
-    lng: LANGUAGE;
-  }>;
-}
-
 const LOGIN_BUTTON_NAME = 'provider';
 
-const LoginPage = async (props: LoginPageProps) => {
-  const params = await props.params;
+const LoginPage = () => {
+  const { t } = useTranslation('login');
+  const { status } = useSession();
 
-  const { t } = await getTranslation('login', params.lng);
+  const handleSignIn = (provider: LOGIN_PROVIDERS) => {
+    assertEnum(LOGIN_PROVIDERS, provider);
+    signIn(provider, { callbackUrl: ROUTES.FRIEND_LIST.pathname });
+  };
+
+  if (status === 'loading') {
+    return <PageLoadingTemplate />;
+  }
+
+  if (status === 'authenticated') {
+    redirect(ROUTES.FRIEND_LIST.pathname);
+  }
 
   return (
     <article className="flex flex-1 flex-col justify-between px-4 py-8">
@@ -25,22 +37,13 @@ const LoginPage = async (props: LoginPageProps) => {
         <Logo className="w-40" />
         <h1 className="ml-2 text-lg font-semibold">{t('title')}</h1>
       </header>
-      <form
-        action={async (formData) => {
-          'use server';
-
-          const provider = formData.get(LOGIN_BUTTON_NAME);
-
-          assertEnum(LOGIN_PROVIDERS, provider);
-
-          await signIn(provider);
-        }}
-      >
+      <div>
         <LoginButton
           fullSize
           name={LOGIN_BUTTON_NAME}
           provider={LOGIN_PROVIDERS.GOOGLE}
           text={t('google-login')}
+          onClick={() => handleSignIn(LOGIN_PROVIDERS.GOOGLE)}
         />
         <div className="flex-center m-4 gap-2 text-slate-300">
           <hr className="flex-1 border-slate-300" />
@@ -52,14 +55,16 @@ const LoginPage = async (props: LoginPageProps) => {
             name={LOGIN_BUTTON_NAME}
             provider={LOGIN_PROVIDERS.KAKAO}
             text={t('kakao-login')}
+            onClick={() => handleSignIn(LOGIN_PROVIDERS.KAKAO)}
           />
           <LoginButton
             name={LOGIN_BUTTON_NAME}
             provider={LOGIN_PROVIDERS.NAVER}
             text={t('naver-login')}
+            onClick={() => handleSignIn(LOGIN_PROVIDERS.NAVER)}
           />
         </section>
-      </form>
+      </div>
     </article>
   );
 };
